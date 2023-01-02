59e1bc43483c432e8ec4a1a0163207c1f6bc5d02
ryugibo <7928632+ryugibo@users.noreply.github.com>
2023-01-03 02:28:47 +0900

    :memo: #5.0 User Interface - 2

--- pomodoro/lib/main.dart
+++ pomodoro/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/screens/home_screen.dart';
 
 void main() {
   runApp(App());
@@ -17,9 +18,7 @@ class App extends StatelessWidget {
         ),
         cardColor: const Color(0xFFF4EDDB),
       ),
-      home: const Scaffold(
-        body: Center(),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
